a1abe7d409032edeeb2ed7dba9c5780910764887
Alejandro Garza <alejandro.garza@acquia.com>
2019-06-07 13:21:19 -0400

    Fix paths to PHP executable/requires

--- auto-ahtpanic-on-tickets/ahtpanic-one-ticket.php
+++ auto-ahtpanic-on-tickets/ahtpanic-one-ticket.php
@@ -1,12 +1,11 @@
-#!/opt/lampp/bin/php
+#!/usr/bin/env php
 <?php
 
 /**
  * Command-line tool to process a single ticket.
  */
-
-require '../zendesk_api.php.inc';
-require 'ZendeskTicketProcessor.php';
+require __DIR__ . '/../zendesk_api.php.inc';
+require __DIR__ . '/ZendeskTicketProcessor.php';
 
 if (empty($argv[1])) {
   echo "Runs the ZendeskTicketProcessorAhtpanicrunner class on one ZD ticket.\n";
