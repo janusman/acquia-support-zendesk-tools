24d9c3788b63e2ef52f9e58f7f5d212096c83dc7
Alejandro Garza <alejandro.garza@acquia.com>
2019-10-15 16:56:08 -0400

    Added installer script

---
+++ auto-ahtpanic-on-tickets/installer-auto-ahtpanic-zendesk.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# installer-auto-ahtpanic-zendesk.sh
+# 
+# 2019-06-14
+#
+#
+# Installer for ticket-watcher-and-auto-ahtpanic
+#
+# This is supposed to install the script in Bastion so that it can run
+# for all of Support. It will ingest ZD tickets and run ahtpanic.sh on
+# them.
+
+INSTALL_FOLDER=$HOME/auto-ahtpanic-zendesk
+
+if [ -r $INSTALL_FOLDER ]
+then
+  echo "Already installed"
+  exit 1
+fi
+  
+echo "Creating folder at $INSTALL_FOLDER"
+
+mkdir $INSTALL_FOLDER
+cd $INSTALL_FOLDER
+
+# Clone git repos
+git clone git@github.com:janusman/Support-Tools.git -b ahtpanic-improvements-2017-11
+git clone git@github.com:janusman/acquia-support-zendesk-tools.git
+
+# Add Zendesk API token.
+echo -n "Enter Zendesk API token: (format = example@acquia.com/token:XXXXXXXXXXX)"
+read token
+echo "$token" >acquia-support-zendesk-tools/creds.txt
+
+## Add some commands to the bin folder
+mkdir bin
+cd bin 
+
+cat <<EOF >aht
+#!/bin/bash
+
+/vol/ebs1/ahsupport/aht/prod/ahtools \$@
+EOF
+cat <<EOF >ahtpanic.sh
+#!/bin/bash
+
+$INSTALL_FOLDER/Support-Tools/bin/ahtpanic.sh \$@
+EOF
+cat <<EOF >run-ticket-watcher
+#!/bin/bash
+
+$INSTALL_FOLDER/acquia-support-zendesk-tools/auto-ahtpanic-on-tickets/run-ticket-watcher
+EOF
+
+curl http://www.pixelbeat.org/scripts/ansi2html.sh >ansi2html
+
+chmod +x ansi2html aht ahtpanic.sh ansi2html run-ticket-watcher
+
+cat <<EOF
+
+================================
+INSTALLATION DONE!
+
+You can run the script this way:
+
+export PATH=$PATH:$INSTALL_FOLDER/bin
+run-ticket-watcher
+EOF
